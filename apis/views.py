9a4292ef050a22f0e0042c6250aef92075c56b17
CybeR-LuduS <139907881+CybeR-LuduS@users.noreply.github.com>
2024-05-04 20:37:27 -0400

    filtro cod_chip

--- apis/views.py
+++ apis/views.py
@@ -8,5 +8,10 @@ from .models import Perro
 
 class PerroViewSet(viewsets.ModelViewSet):
     queryset = Perro.objects.all()
-    
     serializer_class = PerroSerializer
+
+    def get_queryset(self):
+        cod_chip = self.request.GET.get('cod_chip')
+        if cod_chip:
+            return Perro.objects.filter(cod_chip=cod_chip)
+        return self.queryset
